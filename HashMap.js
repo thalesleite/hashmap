a333299fbd4dfae5fde146f71ba7df24e2aff4b6
thalesleite <thalesaleite@gmail.com>
2024-07-12 11:05:34 +0100

    finish the hashmap and add some comments with time and space complexity

--- HashMap.js
+++ HashMap.js
@@ -2,41 +2,178 @@ class HashMap {
   // A prime number size helps in achieving a more uniform distribution of keys.
   // When the size of the array is a prime number, it minimizes the chance that patterns
   // in the data will cause multiple keys to hash to the same index.
-  constructor(size = 53) {
-    this.buckets = new Array(size)
+  constructor() {
+    this.buckets = new Array(16).fill(null).map(() => [])
+    this.capacity = this.buckets.length
+    this.size = 0
+    this.loadFactor = 0.75
   }
 
   hash(key) {
     let hashCode = 0
-
-    // In some collision resolution strategies like open addressing,
-    // using a prime number helps in reducing secondary clustering,
-    // which occurs when clusters of occupied slots form and lead to increased collisions.
     const primeNumber = 31
-    for (let i = 0; index < key.length; i++) {
+    for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i)
     }
 
-    return hashCode
+    return hashCode % this.buckets.length
   }
 
   set(key, value) {
-    let index = this.hash(key)
+    const index = this.hash(key)
 
     if (!this.buckets[index]) {
       this.buckets[index] = []
     }
 
-    for (let i = 0; i < this.buckets[index].length; i++) {
+    const bucket = this.buckets[index]
+
+    for (let i = 0; i < bucket.length; i++) {
       // If a key already exists, then the old value is overwritten
       // or we can say that we update the key’s value
-      if (this.buckets[index][i][0] === key) {
-        this.buckets[index][i][1] = value
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value
         return
       }
     }
 
-    this.buckets[index].push([key, value])
+    bucket.push([key, value])
+    this.size++
+
+    // Check load factor and resize if necessary | Dynamic resizing
+    if (this.size / this.capacity > this.loadFactor) {
+      this.resize()
+    }
+  }
+
+  // get(key) takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null.
+  get(key) {
+    let index = this.hash(key)
+
+    if (!this.buckets[index]) {
+      return null
+    }
+
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === key) {
+        return this.buckets[index][i][1]
+      }
+    }
+  }
+
+  // has(key) takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
+  has(key) {
+    const index = this.hash(key)
+    const bucket = this.buckets[index]
+    if (!bucket) {
+      return false
+    }
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return true
+      }
+    }
+    return false
+  }
+
+  // remove(key) takes a key as an argument. If the given key is in the hash map, it should remove the entry with that key and return true. If the key isn’t in the hash map, it should return false.
+  remove(key) {
+    const index = this.hash(key)
+    const bucket = this.buckets[index]
+    if (!bucket) {
+      return false
+    }
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1)
+        this.size--
+        return true
+      }
+    }
+    return false
+  }
+
+  // length() returns the number of stored keys in the hash map.
+  length() {
+    return this.size
+  }
+
+  // clear() removes all entries in the hash map.
+  clear() {
+    this.buckets = new Array(this.buckets.length)
+    for (let i = 0; i < this.buckets.length; i++) {
+      this.buckets[i] = []
+    }
+    this.size = 0
+  }
+
+  // keys() returns an array containing all the keys inside the hash map.
+  keys() {
+    const keys = []
+    for (const bucket of this.buckets) {
+      if (!bucket) continue
+      for (const [key, value] of bucket) {
+        keys.push(key)
+      }
+    }
+    return keys
+  }
+
+  // values() returns an array containing all the values.
+  values() {
+    const values = []
+    for (let i = 0; i < this.buckets.length; i++) {
+      if (this.buckets[i]) {
+        for (let j = 0; j < this.buckets[i].length; j++) {
+          values.push(this.buckets[i][j][1])
+        }
+      }
+    }
+    return values
+  }
+
+  // entries() returns an array that contains each key, value pair. Example: [[firstKey, firstValue], [secondKey, secondValue]]
+  entries() {
+    const entries = []
+    for (const bucket of this.buckets) {
+      if (!bucket) continue
+      for (const [key, value] of bucket) {
+        entries.push([key, value])
+      }
+    }
+    return entries
+  }
+
+  resize() {
+    const oldBuckets = this.buckets
+    this.capacity = this._getNextPrime(this.capacity * 2)
+    this.buckets = new Array(this.capacity).fill(null).map(() => [])
+    this.size = 0
+
+    for (const bucket of oldBuckets) {
+      for (const [key, value] of bucket) {
+        this.set(key, value)
+      }
+    }
+  }
+
+  // prefixing function or variable names with an underscore _ is a common convention used
+  // to indicate that these functions or variables are intended to be private or for internal use only
+  _getNextPrime(num) {
+    while (!this._isPrime(num)) {
+      num++
+    }
+    return num
+  }
+
+  _isPrime(num) {
+    if (num <= 1) return false
+    if (num <= 3) return true
+    if (num % 2 === 0 || num % 3 === 0) return false
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false
+    }
+    return true
   }
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+/**
+ * Hash Table (also known as hash map, map, dictionary or obj) is a data structure that helps with mapping
+ * keys to values for highly efficient operations like the lookup, insertion and deletion operations.
+ *
+ * Time complexity:
+ * hash(key):       O(n)
+ * set(key, value): O(1)
+ * get(key):        O(1)
+ * has(key):        O(1)
+ * remove(key):     O(1)
+ * values():        O(m+n), where m is the capacity of the hash table and n is the number of elements.
+ *
+ * Space complexity: O(n)
+ */
 const HashMap = require("./HashMap")
 
 const myHashMap = new HashMap()
+
+myHashMap.set("apple", "red")
+myHashMap.set("banana", "yellow")
+myHashMap.set("carrot", "orange")
+myHashMap.set("dog", "brown")
+myHashMap.set("elephant", "gray")
+myHashMap.set("frog", "green")
+myHashMap.set("grape", "purple")
+myHashMap.set("hat", "black")
+myHashMap.set("ice cream", "white")
+myHashMap.set("jacket", "blue")
+myHashMap.set("kite", "pink")
+myHashMap.set("lion", "golden")
+
+console.log(myHashMap.get("grape")) // Output: purple
+console.log(myHashMap.has("kite")) // Output: true
+console.log(myHashMap.remove("lion")) // Output: true
+console.log(myHashMap.length()) // Output: 11
+myHashMap.clear()
+console.log(myHashMap.length()) // Output: 0
+
+myHashMap.set("city", "New York")
+console.log(myHashMap.keys()) // Output: ['city']
+console.log(myHashMap.values()) // Output: ['New York']
+console.log(myHashMap.entries())
